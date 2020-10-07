301ba39114fe075262cbb9222b52a9ead8dc7635
bill-orange <billwebb12@gmail.com>
2020-10-07 07:37:21 -0700

    PROBE_OFFSET_WIZARD

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1091,9 +1091,9 @@
 
   // Add Probe Z Offset calibration to the Bed Leveling menu
   #if HAS_BED_PROBE
-    //#define PROBE_OFFSET_WIZARD
+    #define PROBE_OFFSET_WIZARD
     #if ENABLED(PROBE_OFFSET_WIZARD)
-      #define PROBE_OFFSET_START -4.0   // Estimated nozzle-to-probe Z offset, plus a little extra
+      #define PROBE_OFFSET_START -1.6   // Estimated nozzle-to-probe Z offset, plus a little extra
     #endif
   #endif
 
