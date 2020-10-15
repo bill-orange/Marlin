0f1be8ee7b8e17e3a1aa2e04e144819972269317
bill-orange <billwebb12@gmail.com>
2020-10-15 16:50:27 -0700

    turn on SD

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1339,7 +1339,7 @@
   #endif
 
   // Add an optimized binary file transfer mode, initiated with 'M28 B1'
-  //#define BINARY_FILE_TRANSFER
+  #define BINARY_FILE_TRANSFER
 
   /**
    * Set this option to one of the following (or the board's defaults apply):
@@ -1350,7 +1350,7 @@
    *
    * :[ 'LCD', 'ONBOARD', 'CUSTOM_CABLE' ]
    */
-  //#define SDCARD_CONNECTION LCD
+  #define SDCARD_CONNECTION ONBOARD
 
 #endif // SDSUPPORT
 
