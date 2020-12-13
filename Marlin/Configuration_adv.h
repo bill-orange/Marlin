4068db01c6b5c3c0553b535b35e048eb636bea9c
bill-orange <billwebb12@gmail.com>
2020-12-12 16:11:02 -0800

    Adj probe area

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1734,23 +1734,27 @@
  * the probe to be unable to reach any points.
  */
 #if PROBE_SELECTED && !IS_KINEMATIC
-  //#define MIN_PROBE_EDGE_LEFT MIN_PROBE_EDGE
-  //#define MIN_PROBE_EDGE_RIGHT MIN_PROBE_EDGE
-  //#define MIN_PROBE_EDGE_FRONT MIN_PROBE_EDGE
-  //#define MIN_PROBE_EDGE_BACK MIN_PROBE_EDGE
-  // WEW 01/20/2020
-  #define PROBING_MARGIN_LEFT 15
-  #define PROBING_MARGIN_RIGHT 15
-  #define PROBING_MARGIN_FRONT 10
-  #define PROBING_MARGIN_BACK 10			   																 														
+  //#define PROBING_MARGIN_LEFT PROBING_MARGIN
+  //#define PROBING_MARGIN_RIGHT PROBING_MARGIN
+  //#define PROBING_MARGIN_FRONT PROBING_MARGIN
+  //#define PROBING_MARGIN_BACK PROBING_MARGIN
+  // WEW 01/20/2020 12/12/2020
+  //#define PROBING_MARGIN_LEFT PROBING_MARGIN
+  //#define PROBING_MARGIN_RIGHT PROBING_MARGIN
+  //#define PROBING_MARGIN_FRONT PROBING_MARGIN
+  //#define PROBING_MARGIN_BACK 3
+  // #define PROBING_MARGIN_LEFT 15
+  //#define PROBING_MARGIN_RIGHT 15
+  //#define PROBING_MARGIN_FRONT 10
+  //#define PROBING_MARGIN_BACK 10			   																 														
 #endif			   
 
 #if EITHER(MESH_BED_LEVELING, AUTO_BED_LEVELING_UBL)
   // Override the mesh area if the automatic (max) area is too large
-  //#define MESH_MIN_X MESH_INSET
-  //#define MESH_MIN_Y MESH_INSET
-  //#define MESH_MAX_X X_BED_SIZE - (MESH_INSET)
-  //#define MESH_MAX_Y Y_BED_SIZE - (MESH_INSET)
+  #define MESH_MIN_X MESH_INSET
+  #define MESH_MIN_Y MESH_INSET
+  #define MESH_MAX_X X_BED_SIZE - (MESH_INSET)
+  #define MESH_MAX_Y Y_BED_SIZE - (MESH_INSET)
 #endif
 
 /**
