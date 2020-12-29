e1a1904e26da6801210975343ae0f01b3c403a09
bill-orange <billwebb12@gmail.com>
2020-12-29 13:51:10 -0800

    Corrected offsets and limits

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -999,7 +999,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 0
+#define PROBING_MARGIN 10
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED (133*60)
@@ -1135,14 +1135,14 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -42
+#define X_MIN_POS -33
 //WEW 07_19_2020
 //#define Y_MIN_POS -10
-#define Y_MIN_POS -15
+#define Y_MIN_POS -4
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE - 10
-#define Z_MAX_POS 230
+#define Y_MAX_POS Y_BED_SIZE
+#define Z_MAX_POS 235
 
 /**
  * Software Endstops
